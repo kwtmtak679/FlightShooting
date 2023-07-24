9544d530b84f8364bb06bd109735d75f7d629a8e
MSI\KAWATA <oic.g23.mituaki.kawata@gmail.com>
2023-07-25 08:59:57 +0900

    ドローンを倒した時のエフェクトを追加

--- Assets/Scripts/DroneBreak.cs
+++ Assets/Scripts/DroneBreak.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-//ドローンを壊すためのスクリプト
+//ドローンを壊すためのスクリプト+壊したときにおこるスクリプト
 public class DroneBreak : MonoBehaviour
 {
     //ゲームディレクターの値を入れる
     GameObject director;
+
+    public void Hakai()
+    {
+        //敵を倒した数に一点プラス
+        this.director.GetComponent<GameDirector>().Gekitui();
+
+
+        Destroy(this.gameObject);
+    }
+
 
     //球が当たった時に消す操作
     public void OnCollisionEnter(Collision atatta)
@@ -15,14 +25,17 @@ public class DroneBreak : MonoBehaviour
         //もしドローンに当たったオブジェクトのTagが「Tama」だったら
         if (atatta.gameObject.tag == "Tama")
         {
+            //パーティクルを表示
+            GetComponent<ParticleSystem>().Play();
+
+
             //当たった球とドローンを消す
             Destroy(atatta.gameObject);
-            Destroy(this.gameObject);
+            Invoke("Hakai", 1.0f);
 
-            //敵を倒した数に一点プラス
-            this.director.GetComponent<GameDirector>().Gekitui();
         }
     }
+
 
     void Start()
     {
