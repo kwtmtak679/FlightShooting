2302f8728a941f2be3fbdc5884fdd3e69bae14f9
MSI\KAWATA <oic.g23.mituaki.kawata@gmail.com>
2023-07-25 13:09:18 +0900

    指定の数ドローンを倒すと、テキストを表示してクリアシーンに移行できるように変更

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 //UIなどを管理するスクリプト
 public class GameDirector : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject finish;
+
     //ポイントを入れるテキスト
     GameObject killmater;
     //ポイントを入れる変数
@@ -15,6 +19,11 @@ public class GameDirector : MonoBehaviour
     {
         //kill変数をプラス1
         ++this.kill;
+    }
+
+    public void Clear()
+    {
+        SceneManager.LoadScene("ClearScene");
     }
 
     void Start()
@@ -28,5 +37,13 @@ public class GameDirector : MonoBehaviour
         //テキストに表示するテキストを定義
         this.killmater.GetComponent<TextMeshProUGUI>().text =
             this.kill.ToString() + "kill";
+
+        //倒した数がクリア目標になったら
+        if(kill==2)
+        {
+            finish.SetActive(true);
+
+            Invoke("Clear", 3.0f);
+        }
     }
 }
